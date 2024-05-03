06b03bd2a50c6de39f56220d85dd1d0d93ebf899
KanjanaPS <kanjana.ph.64@ubu.ac.th>
2024-05-03 20:29:59 +0700

    ตกแต่งนิดหน่อย

--- Myweb/settings.py
+++ Myweb/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "app_users",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "MBTI",
-    "app_users",
+    
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "th"
 
 TIME_ZONE = 'Asia/Bangkok'
 
